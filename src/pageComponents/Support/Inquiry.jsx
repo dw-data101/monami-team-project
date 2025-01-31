deff16ea6842e1e4f7a3aca6c84c7617cf7ea417
wonbinwin <youngkf@naver.com>
2025-01-31 10:51:55 +0900

    kyh

--- src/pageComponents/Support/Inquiry.jsx
+++ src/pageComponents/Support/Inquiry.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Inquiry = () => {
-  return <div>Inquiry</div>;
+  return <div>CONTACT US</div>;
+  <div> 문의 내용</div>;
+  <span>▼</span>;
 };
 
 export default Inquiry;

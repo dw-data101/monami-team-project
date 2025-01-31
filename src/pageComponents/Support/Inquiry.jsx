484071ae82a1d7f1166185ac4ee861dd190c1d1a
wonbinwin <youngkf@naver.com>
2025-01-31 10:58:53 +0900

    kyh

--- src/pageComponents/Support/Inquiry.jsx
+++ src/pageComponents/Support/Inquiry.jsx
@@ -4,6 +4,17 @@ const Inquiry = () => {
   return <div>CONTACT US</div>;
   <div> 문의 내용</div>;
   <span>▼</span>;
-};
+  <div>*이름</div>;
+  <div>*이메일
+<input placeholder= 'Google 정책으로 인해 Gmail의 경우 답변 회신을 받지 못할 수도 있으니, 다른 도메인의 메일로 입력부탁드립니다'></input>
+</div>
+<div>연락처</div>
+<div>*제목</div>
+<div>*내용</div>
+<div>첨부파일
+  <button>파일선택</button>
+</div>
+
+}
 
 export default Inquiry;
